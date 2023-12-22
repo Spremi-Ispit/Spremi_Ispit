17abfaef9cd5e20775df9e68f6e45004cc83e895
darjan <darjan.drugarinovic@cubic.com>
2023-12-22 18:20:07 +0100

    updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import colors from '../theme/colors';
 import CopyrightIcon from '@mui/icons-material/Copyright';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const FooterDiv = styled.footer`
   background-color: ${colors.footer};
@@ -13,11 +13,6 @@ const FooterDiv = styled.footer`
 
 const CaregoriesDiv = styled.div`
   display: flex;
-`;
-
-const StyledNavlink = styled(NavLink)`
-  margin: 10px;
-  text-transform: none;
 `;
 
 const CopyRightDiv = styled.div`
@@ -31,17 +26,23 @@ const CopyRightDiv = styled.div`
   background-color: ${colors.filteri};
 `;
 
-const AboutDiv = styled.div`
+const SectionDiv = styled.div`
   border-right: 1px solid black;
   flex: 1;
   display: flex;
-  justify-content: center;
   flex-direction: column;
   text-align: justify;
   padding: 10px;
-  align-items: center;
   padding-left: 50px;
   padding-right: 50px;
+`;
+
+const LeftDiv = styled(SectionDiv)``;
+
+const MiddleDiv = styled(SectionDiv)``;
+
+const RightDiv = styled(SectionDiv)`
+  align-items: center;
 `;
 
 const HeaderDiv = styled.div`
@@ -49,84 +50,76 @@ const HeaderDiv = styled.div`
   font-weight: bold;
   margin-bottom: 10px;
   text-transform: uppercase;
-`;
-
-const JoinDiv = styled.div`
-  padding: 10px;
-  flex: 1;
-  display: flex;
-  box-shadow: rgba(0, 0, 0, 0.75) 0px 1px 1px 0px;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-`;
-const BugsDiv = styled.div`
-  padding: 10px;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const WantToDiv = styled.div`
-  padding: 10px;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  text-align: center;
 `;
 
 export const Footer = () => {
   return (
     <FooterDiv>
       <CaregoriesDiv>
-        <AboutDiv>
+        <LeftDiv>
           <HeaderDiv>O projektu</HeaderDiv>
           Ovo je open-source projekat koji razvijaju studenti da bi olaksali
           pripremu/polaganje ispita. Cilj platforme je da olaksa deljenje
           materijala, postavljanje blanketa i resenja, beleski sa racunskih
           vezbi i predavanja kako bi svi materijali za pripremu ispita bili na
           jednom mestu.
-        </AboutDiv>
-        <JoinDiv>
-          <HeaderDiv>Najbolji materijali na jednom mestu!</HeaderDiv>U situaciji
-          u kojoj studenti često nailaze na nedostatak kvalitetnih materijala,
-          često se suočavaju sa izazovima u vezi sa polaganjem ispita, uzimajuci
-          privatne casove. Umesto da troše novac na privatne časove radi
-          pristupa resenim materijalima, naš sajt nudi jednostavno rešenje - svi
-          najbolji materijali, ocenjeni prema kvalitetu, su na jednom mestu.
-        </JoinDiv>
-        <BugsDiv>
-          <HeaderDiv>
-            Zelis da se prikljucis razvoju?
-            <StyledNavlink>Kontaktiraj nas!</StyledNavlink>
-          </HeaderDiv>
-
-          <WantToDiv>
-            <HeaderDiv>Zelis da:</HeaderDiv>
-            <ul>
-              <li>
-                <NavLink
-                  as="a"
-                  href="https://docs.google.com/document/d/1AefnDWibn1vOFxZAfTC9L-vOkv2DqdEr94SYz4Nx_4o/edit?usp=sharing"
-                  target="_blank"
-                >
-                  Prijavis bag/predlozis feature
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  as="a"
-                  href="https://trello.com/b/gF0nh6ti/spremiispit"
-                  target="_blank"
-                >
-                  Vidis na cemu se trenutno radi
-                </NavLink>
-              </li>
-            </ul>
-          </WantToDiv>
-        </BugsDiv>
+        </LeftDiv>
+        <MiddleDiv>
+          <HeaderDiv>Najbolji materijali na jednom mestu!</HeaderDiv>
+          Umesto da trošite novac na privatne časove radi pristupa resenim
+          materijalima, naš sajt nudi jednostavno rešenje - svi najbolji
+          materijali, ocenjeni prema kvalitetu, su na jednom mestu!
+        </MiddleDiv>
+        <RightDiv>
+          <HeaderDiv>Zelis da budes deo projekta?</HeaderDiv>
+          <ul>
+            <li>
+              <a
+                as="a"
+                href="https://docs.google.com/document/d/1AefnDWibn1vOFxZAfTC9L-vOkv2DqdEr94SYz4Nx_4o/edit?usp=sharing"
+                target="_blank"
+              >
+                🐞Prijavi bag
+              </a>
+            </li>
+            <li>
+              <a
+                as="a"
+                href="https://docs.google.com/document/d/1AefnDWibn1vOFxZAfTC9L-vOkv2DqdEr94SYz4Nx_4o/edit?usp=sharing"
+                target="_blank"
+              >
+                💡Predlozi feature
+              </a>
+            </li>
+            <li>
+              <a
+                as="a"
+                href="https://trello.com/b/gF0nh6ti/spremiispit"
+                target="_blank"
+              >
+                🛠️Vidi na cemu se trenutno radi
+              </a>
+            </li>
+            <li>
+              <a
+                as="a"
+                href="https://docs.google.com/document/d/1ktu2u97ZVWWkA9iWx_NgcXP7n91FJ2GnkX14wuvooW0/edit?usp=sharing"
+                target="_blank"
+              >
+                📞Kontaktiraj nas
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Spremi-Ispit/Spremi_Ispit"
+                target="_blank"
+              >
+                💻Github
+              </a>
+            </li>
+          </ul>
+        </RightDiv>
       </CaregoriesDiv>
       <CopyRightDiv>
         <CopyrightIcon /> 2023
