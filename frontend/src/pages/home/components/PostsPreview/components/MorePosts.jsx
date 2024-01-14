2b2964737bbcdac23ddc6f1df5c64345f4e56298
darjan <darjan.drugarinovic@cubic.com>
2024-01-14 17:26:25 +0100

    updated default style for button

--- frontend/src/pages/home/components/PostsPreview/components/MorePosts.jsx
+++ frontend/src/pages/home/components/PostsPreview/components/MorePosts.jsx
@@ -6,6 +6,7 @@ import colors from '../../../../../theme/colors';
 const MorePostsDiv = styled.div`
   display: flex;
   justify-content: center;
+  margin-top: 20px;
 `;
 
 const MorePostsButton = styled(Button)`
