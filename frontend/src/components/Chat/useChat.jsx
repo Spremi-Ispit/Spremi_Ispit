780f1e9644ce6bb00733612d95a52ec42c90315c
darjan <darjan.drugarinovic@cubic.com>
2024-01-30 00:56:07 +0100

    updated env secrets for chat

--- frontend/src/components/Chat/useChat.jsx
+++ frontend/src/components/Chat/useChat.jsx
@@ -4,7 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_CHAT_CONFIG);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_CHAT_API_KEY,
+  authDomain: import.meta.env.VITE_CHAT_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_CHAT_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_CHAT_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_CHAT_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_CHAT_APP_ID,
+  measurementId: import.meta.env.VITE_CHAT_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
