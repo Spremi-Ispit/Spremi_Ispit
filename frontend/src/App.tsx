c3667ce436d34182da8568479e0f51a883ef0732
darjan <darjan.drugarinovic@cubic.com>
2024-03-10 17:51:05 +0100

    Testing analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,21 @@ import ErrorBoundary from './components/ErrorBoundary';
 import useCorrectAppVersion from './utils/useCorrectAppVersion';
 import Router from './router/Router';
 import ReactGA from 'react-ga4';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 ReactGA.initialize('G-J24NM9G7SY');
 
-ReactGA.send({
-  hitType: 'pageview',
-  page: window.location.pathname,
-});
-
 function App() {
   useCorrectAppVersion();
+  const location = useLocation();
 
-  // let location = useLocation();
-
-  // useEffect(() => {
-  //   ReactGA.send('pageview', location.pathname);
-  // }, [location]);
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: window.location.pathname,
+    });
+  }, [location]);
 
   return (
     <>
