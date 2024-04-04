cc851f08dc5e16a0a80294b3b1205406ca9d4d5b
darjan <darjan.drugarinovic@cubic.com>
2024-04-04 23:41:56 +0200

    updated default password for user in test data

--- backend/database/test_data/01_user.sql
+++ backend/database/test_data/01_user.sql
@@ -1,11 +1,9 @@
-
-
 INSERT INTO `user` (`username`, `email`, `password`, `role`) VALUES
-('Admin', 'admin@spremiispit.com','$2b$10$w1gSmaNYIDSH.xMGYF5OZOXTsIfskuW4BSlVmFR9yepzWDR/jvOIa', 'admin'), -- password = 123
-('a','a@elfak.rs','$2b$10$Mr4ZX.U07sw..Yk71WQu.uqreE9HQ6YCnFfMLnAOlSRXrT5NrEHwm','visitor'),
-('b','b@elfak.rs','$2b$10$wHvK950voZQ4tqZEUdJyLeKU.PEn/VbCNOkMb0.WUNOs2amZPDFS2','visitor'),
-('c','c@elfak.rs','$2b$10$qlqFhmY25PhPyP0daaRsSO7ZYlrubtQIZY4a7h4OqaOz3gmariSHe','visitor'),
-('d','d@elfak.rs','$2b$10$mok1UWGh9T4Rb4c6pTqPp.w1J8qujj6LMftKRUXPQG/vABI8fclGu','visitor');
+('Admin', 'admin@spremiispit.com','$2b$10$m6h31jtlXT6L3M/uwory8OjhE9gDMpIuTjYMo2P84jltANjBZsPDC', 'admin'), -- password = P@ssw0rd1234
+('a','a@elfak.rs','$2b$10$m6h31jtlXT6L3M/uwory8OjhE9gDMpIuTjYMo2P84jltANjBZsPDC','visitor'),
+('b','b@elfak.rs','$2b$10$m6h31jtlXT6L3M/uwory8OjhE9gDMpIuTjYMo2P84jltANjBZsPDC','visitor'),
+('c','c@elfak.rs','$2b$10$m6h31jtlXT6L3M/uwory8OjhE9gDMpIuTjYMo2P84jltANjBZsPDC','visitor'),
+('d','d@elfak.rs','$2b$10$m6h31jtlXT6L3M/uwory8OjhE9gDMpIuTjYMo2P84jltANjBZsPDC','visitor');
 
 UPDATE user
 SET banned = true
