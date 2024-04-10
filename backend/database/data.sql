06e46486a813b0224ee7d85cf58a128282271d06
darjan <darjan.drugarinovic@cubic.com>
2024-04-10 22:24:26 +0200

    updated database test data

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -1041,7 +1041,7 @@ INSERT INTO `tutor_tutoring_subjects_subject` (`tutorId`, `subjectId`) VALUES
 INSERT INTO `tutoring_request` (`message`, `isFinished`, `isCanceled`, `rating`, `tutorId`, `studentId`, `subjectId`) VALUES
 ('Meni jako treba pomoc da spremim lab vezbe is ovog predmeta. Imam neko osnovno znanje ali zelim da naucim nesto vise.',0,0,0,1,4,1),
 ('Zelim pripremu za ispit, nista ne znam i morali bi iz pocetka sve da predjemo.',0,0,0,1,5,23),
-('Potrebna mi je pomoc HITNO, nista ne znam a 20.04 mi je ispit',0,0,0,8,1,15),
-('Nista ne znam majke mi, treba mi pomoc. Kako da naucim sve ovo za ispit za 5 dana? Da li mozete da mi odradite ispit?',0,1,-1,8,1,12),
+('Potrebna mi je pomoc HITNO, nista ne znam a 20.04 mi je ispit',0,0,0,2,1,15),
+('Nista ne znam majke mi, treba mi pomoc. Kako da naucim sve ovo za ispit za 5 dana? Da li mozete da mi odradite ispit?',0,1,-1,2,1,12),
 ('Voleo bih da naucim sve za junski ispitni rok, i sve labove da odradim kako treba!',1,0,1,1,5,10);
 
