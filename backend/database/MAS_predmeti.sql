b899de56cd0b166d007f9dca984818c46e585bfa
DarjanDrugarinovic <darjandrugarinovic@gmail.com>
2024-06-06 23:25:44 +0200

    added subjects for faculty of mechanical engineering

---
+++ backend/database/MAS_predmeti.sql
@@ -0,0 +1,147 @@
+Mašinske konstrukcije, razvoj i inženjering
+Proizvodno informacione tehnologije
+Energetika i procesna tehnika
+Mehatronika i upravljanje
+Saobraćaj, transport i logistika
+
+INSERT INTO `subject`(`name`) VALUES
+-- I GODINA
+Matematika 1 
+Mehanika 1 - Statika
+Fizika
+Informaciono-komunikacione tehnologije
+Sociologija kulture i morala
+Pogonski materijali
+Matematika 2 
+Elektrotehnika sa elektronikom
+Otpornost materijala
+Mašinski materijali
+Inženjerska grafika
+-- II GODINA
+Mehanika 2 - Kinematika
+Engleski jezik 1
+Nemački jezik 1
+Ruski jezik 1
+Mašinski elementi 1
+Matematika 3
+Proizvodne tehnologije
+Programiranje
+Engleski jezik 2
+Nemački jezik 2
+Ruski jezik 2
+Mehanika 3 - Dinamika
+Konstruisanje primenom računara
+Mašinski elementi 2 
+Profesionalna etika inženjera
+Osnove inženjerskog menadžmenta
+-- III GODINA
+Termodinamika -> SVI
+Mehanika fluida -> SVI
+Teorija oscilacija -> SVI
+Teorija elastičnosti -> SVI
+Otpornost konstrukcija -> SVI
+Numerička matematika -> SVI
+Diskretna matematika -> SVI
+Statistika -> SVI
+Strujno-tehnička merenja -> Energetika i procesna tehnika
+Merna tehnika -> ???
+Merenje i kontrola -> Mašinske konstrukcije, razvoj i inženjering; Proizvodno informacione tehnologije
+Primenjena termodinamika -> Energetika i procesna tehnika
+Primenjena mehanika fluida -> Energetika i procesna tehnika
+Transportni tokovi -> Saobraćaj, transport i logistika
+Osnove konstruisanja -> Mašinske konstrukcije, razvoj i inženjering
+Mehanizmi i mašine -> Mehatronika i upravljanje
+Tehnologije obrade rezanjem -> Proizvodno informacione tehnologije
+Obnovljivi izvori energije -> Energetika i procesna tehnika
+Tehnička logistika -> Saobraćaj, transport i logistika
+Metoda konačnih elemenata u strukturnoj analizi -> Mašinske konstrukcije, razvoj i inženjering
+Tribologija -> Mašinske konstrukcije, razvoj i inženjering
+Optički elementi u mehatronici -> Mehatronika i upravljanje
+Tehnologije plastičnog deformisanja -> Proizvodno informacione tehnologije
+Osnove turbomašina -> Energetika i procesna tehnika
+Osnove saobraćaja i transporta  -> Saobraćaj, transport i logistika
+Prenosnici snage -> Mašinske konstrukcije, razvoj i inženjering
+Elektromehanika i elektronika -> Mehatronika i upravljanje
+Nekonvencionalne obrade -> Proizvodno informacione tehnologije
+Poslovni informacioni sistemi -> Proizvodno informacione tehnologije
+Toplotna postrojenja  -> Energetika i procesna tehnika
+Energija i okolina  -> Energetika i procesna tehnika
+Hidropneumatski sistemi -> Saobraćaj, transport i logistika
+Pogonski sistemi -> Saobraćaj, transport i logistika
+Tehnologija zavarivanja -> Mašinske konstrukcije, razvoj i inženjering
+Mehatronika -> Mehatronika i upravljanje
+Aditivne tehnologije -> Proizvodno informacione tehnologije
+-- IV GODINA
+Upravljanje sistemima -> SVI
+Grejanje -> Energetika i procesna tehnika
+Osnove procesne tehnike -> Energetika i procesna tehnika
+Kompresori i ventilatori -> Energetika i procesna tehnika
+Projektovanje mobilnih mašina -> Saobraćaj, transport i logistika
+Organizacija i tehnologija drumskog saobraćaja -> Saobraćaj, transport i logistika
+Kvalitet mašinskih sistema -> Mašinske konstrukcije, razvoj i inženjering
+Hidraulični i pneumatski sistemi upravljanja -> Mehatronika i upravljanje
+Proizvodna sredstva -> Proizvodno informacione tehnologije
+Kotlovi -> Energetika i procesna tehnika
+Industrijske peći -> Energetika i procesna tehnika
+Komunalna hidrotehnika i navodnjavanje -> Energetika i procesna tehnika
+Sistemi neprekidnog transporta -> Saobraćaj, transport i logistika
+Drumska vozila -> Saobraćaj, transport i logistika
+Pakovanje i paletizacija -> Saobraćaj, transport i logistika
+Virtuelno konstruisanje -> Mašinske konstrukcije, razvoj i inženjering
+Modeliranje i simulacija mehatroničkih sistema -> Mehatronika i upravljanje
+Automatizacija proizvodnje -> Proizvodno informacione tehnologije
+Primena MKE -> Proizvodno informacione tehnologije
+Tehnika hlađenja -> Energetika i procesna tehnika
+Zaštita životne sredine i održivi razvoj -> Energetika i procesna tehnika
+Radne karakteristike i regulacija turbomašina -> Energetika i procesna tehnika
+Logistika preduzeća  -> Saobraćaj, transport i logistika
+Mašine prekidnog transporta -> Saobraćaj, transport i logistika
+Intermodalni transport  -> Saobraćaj, transport i logistika
+Tehnologičnost -> ???
+Alati i pribori -> Proizvodno informacione tehnologije
+Stručna praksa B -> SVI
+Cevni vodovi -> Energetika i procesna tehnika
+Toplotne operacije i aparati -> Energetika i procesna tehnika
+Hidroenergetska postrojenja i oprema -> Energetika i procesna tehnika
+Strukturna analiza mašina i vozila -> Saobraćaj, transport i logistika
+Transportne mreže -> Saobraćaj, transport i logistika
+Održavanje transportnih sredstava -> Saobraćaj, transport i logistika
+Integritet konstrukcija -> Mašinske konstrukcije, razvoj i inženjering
+Industrijska automatika -> Mehatronika i upravljanje
+Planiranje tehnoloških procesa -> Proizvodno informacione tehnologije
+Klimatizacija i provetravanje -> Energetika i procesna tehnika
+Toplotne turbomašine -> Energetika i procesna tehnika
+Mehaničke i hidromehaničke operacije -> Mehatronika i upravljanje
+Hidraulične mašine -> Mehatronika i upravljanje
+Skladišna tehnika -> Saobraćaj, transport i logistika
+Alternativni pogoni motornih vozila -> Saobraćaj, transport i logistika
+Vazduhoplovna prevozna sredstva -> Saobraćaj, transport i logistika
+Osnove razvoja proizvoda -> Mašinske konstrukcije, razvoj i inženjering
+Napredno geometrijsko modeliranje -> Proizvodno informacione tehnologije
+Odabrana poglavlja iz softverskog inženjerstva -> Proizvodno informacione tehnologije
+Motori SUS i hibridni sistemi -> Energetika i procesna tehnika
+Tretman čvrstog otpada -> Energetika i procesna tehnika
+Konstruisanje procesnih aparata i uređaja -> Energetika i procesna tehnika
+Gasna tehnika -> Energetika i procesna tehnika
+Termoenergetska postrojenja -> Energetika i procesna tehnika
+Sušare -> Energetika i procesna tehnika
+Proračunska dinamika fluida -> Energetika i procesna tehnika
+Hidroprenosnici snage -> Energetika i procesna tehnika
+Uljna hidraulika i pneumatika -> Mehatronika i upravljanje
+Projektovanje energetskih sistema primenom računara -> Energetika i procesna tehnika
+Urbani transport i logistika -> Saobraćaj, transport i logistika
+Iinteligentni transportni sistemi -> Saobraćaj, transport i logistika
+Aplikativni softver u transportu i logistici -> Saobraćaj, transport i logistika
+Ergonomija i industrijski dizajn -> Mašinske konstrukcije, razvoj i inženjering
+Tehnička dijagnostika -> Mašinske konstrukcije, razvoj i inženjering
+Železnička vozila -> Mašinske konstrukcije, razvoj i inženjering
+Mašinsko učenje -> Mehatronika i upravljanje
+Inteligentno upravljanje -> Mehatronika i upravljanje
+Računarom podržano upravljanje sistemima -> Mehatronika i upravljanje
+Internet stvari -> Proizvodno informacione tehnologije
+Tehnologija montaže -> Proizvodno informacione tehnologije
+Programiranje NUMA 1 -> Proizvodno informacione tehnologije
+Upravljanje kvalitetom -> Proizvodno informacione tehnologije
+Tehnologije reciklaže -> Proizvodno informacione tehnologije
+Završni rad - istraživački rad na teorijskim osnovama diplomskog rada -> SVI
+Završni rad - izrada i odbrana diplomskog rada -> SVI
