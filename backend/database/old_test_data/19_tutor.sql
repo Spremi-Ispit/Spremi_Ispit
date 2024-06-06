176bb688aa2e3ac123f6264409d8899cf6959c6b
DarjanDrugarinovic <darjandrugarinovic@gmail.com>
2024-06-06 21:38:03 +0200

    added old test data

---
+++ backend/database/old_test_data/19_tutor.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO `tutor` (`id`, `price`, `groupPrice`, `isEnabled`, `message`, `phone`, `likes`, `dislikes`, `name`, `link`) VALUES 
+(1,1500,700,1,'SVE ZNAM dodji na cas','123',0,0, "A",""),
+(2,1200,800,1,'Nastava za predmete','456',0,0,"B","");
